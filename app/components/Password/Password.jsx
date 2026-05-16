7664655ea302d1825f590318b42a77e556c393ff
Chris <christophereko@hotmail.fr>
2026-05-16 13:35:25 +0100

    Added some functions to store and validate the username and password

--- app/components/Password/Password.jsx
+++ app/components/Password/Password.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
 
 function Password() {
+
+  const [password, setPassword] = useState("");
+  const [passwordValidity, setPasswordValidity] = useState(false);
+
+  function storePassword (event) {
+    let passwordValue = event.target.value;
+
+    setPassword(passwordValue);
+  }
+
+  function isPasswordValid () {
+    if (password.length >= 12) {
+      setPasswordValidity(true);
+    } else {
+      setPasswordValidity(false);
+    }
+  }
+
   return (
     <div>
-        <label for="password" class="block text-sm/6 font-semibold text-gray-900">Password</label>
+        <label htmlFor="password" class="block text-sm/6 font-semibold text-gray-900">Password</label>
         <div class="mt-2.5">
-          <input id="password" type="text" name="password" placeholder="password" autocomplete="password" class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600" />
+          <input onChange={(e) => storePassword(e)} id="password" type="text" name="password" placeholder="password" autocomplete="password" class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600" />
         </div>
       </div>
   )
--- app/components/Username/Username.jsx
+++ app/components/Username/Username.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react';
 
 function Username() {
+
+  const [username, setUsername] = useState("");
+  const [usernameValidity, setUsernameValidity] = useState(false);
+
+  function saveUsername (event) {
+    let usernameValue = event.target.value;
+    
+    setUsername(usernameValue);
+  }
+  
+  function validateUsername () {
+    if (username.length >= 3) {
+      setUsernameValidity(true);
+    } else {
+      setUsernameValidity(false);
+    }
+  }
+
   return (
     <div>
         <label for="user-name" class="block text-sm/6 font-semibold text-gray-900">User name</label>
         <div class="mt-2.5">
-          <input id="user-name" type="text" name="user-name" placeholder="user name" autocomplete="given-name" class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600" />
+          <input onChange={(e) => storeUsername(e)} id="user-name" type="text" name="user-name" placeholder="user name" autocomplete="given-name" class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600" />
         </div>
       </div>
   )
